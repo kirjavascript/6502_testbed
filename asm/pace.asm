c66dd65f01a8d3e1972cad05c3278992f7f43119
kirjavascript <snkenjoi@gmail.com>
2020-12-18 23:21:49 +0000

    threshold stuff

--- asm/pace.asm
+++ asm/pace.asm
@@ -1,22 +1,28 @@
 lines := $10
 target := $20
+marker := $F0
 
+lineTargetThreshold := 110
 
 main:
 
-    lda lines
-    cmp #110
-    bcc @baseTarget
+        lda lines
+        cmp #lineTargetThreshold
+        bcc @baseTarget
 
+
+        jmp @done
 
 @baseTarget:
-    lda targetTable
-    sta target
-    lda targetTable
-    sta target+1
-
+        lda targetTable
+        sta target
+        lda targetTable
+        sta target+1
 @done:
 
+
+        lda #$FF
+        sta marker
 
 targetTable:
         .byte $A0,$0F
