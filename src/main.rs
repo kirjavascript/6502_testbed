1bd1b5c4799a8388e79d219287abe940173df97e
kirjavascript <snkenjoi@gmail.com>
2021-12-12 01:10:09 +0000

    fix bcd in pushdown calculation

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,27 @@ mod emu;
 
 fn main() {
 
-use emulator_6502::Interface6502;
+    // TODO: handle BCD
+    for score in 0..50 {
+
+        pushdown(0xC, score);
+    }
+
+}
+
+fn dec_bcd(val: u8) -> u8 {
+    (val/10*16) + (val%10)
+}
+
+fn pushdown(pushdown: u8, score: u8) {
+
+    use emulator_6502::Interface6502;
 
     let (mut cpu, mut ram) = emu::load();
 
-    ram.write(0x01, 0xC); // pushdown
-    ram.write(0x02, 0x0); // score
+    ram.write(0x01, pushdown); // pushdown
+    ram.write(0x02, dec_bcd(score)); // score
+    ram.write(0x03, 0); // score
 
     loop {
         cpu.execute_instruction(&mut ram);
@@ -20,8 +35,10 @@ use emulator_6502::Interface6502;
     }
 
 
-    println!("pdp {:x} high {:x} low {:x}",
+    println!("pdp {} s0: {}-{:x} high {:x} low {:x}",
         ram.read(0x01),
+        score,
+        score,
         ram.read(0x03),
         ram.read(0x02),
     );
