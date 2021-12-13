d4c67c7d8e126580c62744b6efd51bced27f2187
kirjavascript <snkenjoi@gmail.com>
2021-12-13 00:18:25 +0000

    added hydrant's implementation

--- src/main.rs
+++ src/main.rs
@@ -4,38 +4,50 @@ mod emu;
 
 fn main() {
 
-    for score in 0..50 {
-        pushdown(23, score);
+    for score in 0..105 {
+        pushdown(13, score);
     }
 
 }
 
-fn _pushdown(pushdown: u8, score: u8) -> u8 {
-    if pushdown > 7 {
-        if pushdown > 16 || score % 10 > 10 - (pushdown - 6) {
-            return pushdown - 7
-        }
+fn _pushdown(pushdown: u8, score: u16) -> u16 {
+    let mut newscore = (score % 10) as u8 + (pushdown - 1);
+    if newscore & 0b1111 > 9 {
+        newscore += 6;
     }
-
-    pushdown - 1
+    ((newscore & 0b11110000) / 16 * 10) as u16 + (newscore & 0b1111) as u16 + (score - (score % 10))
 }
 
-fn dec_bcd(val: u8) -> u8 {
+// fn _pushdown(pushdown: u8, score: u8) -> u8 {
+//     if pushdown > 7 {
+//         if pushdown > 16 || score % 10 > 10 - (pushdown - 6) {
+//             return pushdown - 7
+//         }
+//     }
+
+//     pushdown - 1
+// }
+
+
+fn dec_bcd(val: u16) -> u16 {
     (val/10*16) + (val%10)
 }
 fn bcd_dec(val: u8) -> u8 {
    (val/16*10) + (val%16)
 }
 
-fn pushdown(pushdown: u8, score: u8) {
+fn pushdown(pushdown: u8, score: u16) {
 
     use emulator_6502::Interface6502;
 
     let (mut cpu, mut ram) = emu::load();
 
     ram.write(0x01, pushdown); // pushdown
-    ram.write(0x02, dec_bcd(score)); // score
-    ram.write(0x03, 0); // score
+    let bcd_high = i64::from_str_radix(&format!("{:04}", score)[0..2], 16).unwrap();
+    let bcd_low = i64::from_str_radix(&format!("{:04}", score)[2..4], 16).unwrap();
+
+    ram.write(0x02, bcd_low as u8);
+    ram.write(0x03, bcd_high as u8);
 
     loop {
         cpu.execute_instruction(&mut ram);
@@ -44,8 +56,18 @@ fn pushdown(pushdown: u8, score: u8) {
             break;
         }
     }
+    let bcd = format!("{:02x}{:02x}", ram.read(0x03), ram.read(0x02));
+    let newScore = bcd.parse::<u16>().unwrap();
 
-    let added = format!("{:x}", ram.read(0x02)).parse::<u8>().unwrap() - score;
+
+    let added = if newScore >= score {
+        format!("{}", newScore - score)
+    } else {
+        format!("{} {}", newScore, score)
+    };
+
+
+    // print!("{}", bcd);
 
     print!("score {} PDP {} Actual PDP {}",
         score,
@@ -55,7 +77,7 @@ fn pushdown(pushdown: u8, score: u8) {
     println!(" -- score {} PDP {} Actual PDP {}",
         score,
         ram.read(0x01),
-        _pushdown(ram.read(0x01), score),
+        _pushdown(ram.read(0x01), score) - score,
     );
 
     // println!("PDP {} Added PDP {} start score {}:{} end score {:x}:{:x} ",
