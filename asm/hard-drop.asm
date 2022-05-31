3a14d6875e1b23487b7d447d4cb13fce55a2736b
kirjavascript <snkenjoi@gmail.com>
2022-05-31 02:28:43 +0100

    delete unused stuff

--- asm/hard-drop.asm
+++ asm/hard-drop.asm
@@ -139,8 +139,6 @@ harddropShift:
         lda multBy10Table, x
         sta lineOffset ; reuse for lineOffset * 10
 
-
-
         ldx tmpY
         lda multBy10Table, x
         sta harddropAddr+0
@@ -158,29 +156,6 @@ harddropShift:
         iny
         cpx #$A
         bne @shiftLineLoop
-
-
-        ; loop*10
-        ; ldy #0
-        ; ldx tmpY
-        ; lda multBy10Table, x
-        ; sta tmpX
-        ; sec
-        ; sbc lineOffset
-        ; sta tmpZ
-
-; @shiftLineLoop:
-
-        ; ldx tmpZ
-        ; lda playfield, x
-        ; ldx tmpX
-        ; sta playfield, x
-
-        ; inc tmpX
-        ; inc tmpZ
-        ; iny
-        ; cpy #$A
-        ; bne @shiftLineLoop
 
 @nextLine:
         dec tmpY
@@ -201,120 +176,6 @@ harddropShift:
         lda #$FF
         sta marker
 
-        ; lda #19
-        ; sta tmpY ; row
-; @lineLoop:
-
-        ; ldx tmpY
-        ; lda multBy10Table, x
-        ; tax
-
-        ; ; check for empty row
-        ; ldy #$0
-; @minoLoop:
-        ; lda playfield, x
-        ; cmp #EMPTY_TILE
-        ; beq @noLineClear
-
-        ; inx
-        ; iny
-        ; cpy #$A
-        ; beq @lineClear
-        ; jmp @minoLoop
-
-; @lineClear:
-        ; inc completedLines
-; @noLineClear:
-        ; lda completedLines
-        ; beq @nextLine
-
-        ; ; get line offset
-        ; lda #0
-        ; sta lineOffset
-        ; lda completedLines
-        ; sta completedLinesCopy
-
-        ; sec
-        ; txa
-        ; sbc #20
-        ; sta tmpZ ; i - 1
-        ; tax
-
-; @offsetLoop:
-        ; ; check for empty row
-        ; ldy #$0
-; @offsetCheckLineFull:
-        ; lda playfield, x
-        ; cmp #EMPTY_TILE
-        ; beq @emptyLine
-
-        ; inx
-        ; iny
-        ; cpy #$A
-        ; beq @fullLine
-        ; jmp @offsetCheckLineFull
-
-; @emptyLine:
-        ; dec completedLinesCopy
-; @fullLine:
-        ; inc lineOffset
-
-        ; lda tmpZ
-        ; sbc #10
-        ; sta tmpZ
-        ; tax
-
-        ; lda completedLinesCopy
-        ; bne @offsetLoop
-
-        ; lda lineOffset
-        ; beq @nextLine
-
-        ; tax
-        ; lda multBy10Table, x
-        ; sta lineOffset ; reuse for lineOffset * 10
-
-        ; ; loop*10
-        ; ldy #0
-        ; ldx tmpY
-        ; lda multBy10Table, x
-        ; sta tmpX
-        ; sec
-        ; sbc lineOffset
-        ; sta tmpZ
-
-; @shiftLineLoop:
-
-        ; ldx tmpZ
-        ; lda playfield, x
-        ; ldx tmpX
-        ; sta playfield, x
-
-        ; inc tmpX
-        ; inc tmpZ
-        ; iny
-        ; cpy #$A
-        ; bne @shiftLineLoop
-
-; @nextLine:
-        ; dec tmpY
-        ; lda tmpY
-        ; cmp #0
-        ; beq @addScore
-        ; jmp @lineLoop
-
-; @addScore:
-
-        ; lda #EMPTY_TILE
-        ; ldx #0
-; @topRowLoop:
-        ; sta playfield, x
-        ; inx
-        ; cpx #$A
-        ; bne @topRowLoop
-
-        ; lda #$FF
-        ; sta marker
 
 multBy10Table:
         .byte   $00,$0A,$14,$1E,$28,$32,$3C,$46
