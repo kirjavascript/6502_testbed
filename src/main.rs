44a3ddb91f68a585a003c9333e224d4ee5b1b5b9
Richard <zohassadar@gmail.com>
2023-04-04 10:35:10 -0700

    Restore main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,60 @@
-mod emu;
-mod line_clear;
+use std::thread;
+use std::time::Duration;
+use std::ops::Range;
 
-fn main() {
-    line_clear::print_line_clear();
-    line_clear::print_line_clear_crash();
-    line_clear::print_line_clear_mod();
+fn search(range: Range<u32>) {
+
+        let mut sps = ntools::gym_sps::GymSPS::new();
+
+        for i in range {
+            for j in 0..=255 {
+                for k in 0..=255 {
+                    sps.reset();
+                    sps.set_input(i as u8, j, k);
+                    if sps.next() == ntools::block::Block::I {
+                        if sps.next() == ntools::block::Block::O {
+                        if sps.next() == ntools::block::Block::L {
+                        if sps.next() == ntools::block::Block::Z {
+                        if sps.next() == ntools::block::Block::T {
+                        if sps.next() == ntools::block::Block::J {
+                        if sps.next() == ntools::block::Block::L {
+                        if sps.next() == ntools::block::Block::T {
+                        if sps.next() == ntools::block::Block::S {
+                        if sps.next() == ntools::block::Block::Z {
+                        println!(
+                            "{:02x}{:02x}{:02x}",
+                            i,
+                            j,
+                            k,
+                        );
+                        }}}}}}}}}
+                    }
+
+                }
+            }
+            println!("{}/255", i);
+        }
 }
 
+fn main() {
+
+    let threads = 8;
+    let size = 255 / threads;
+    let mut count = 0;
+
+    loop {
+
+        if count > 255 { break; }
+
+        let start = count;
+
+        thread::spawn(move || {
+            search(start..start+size);
+        });
+
+        count += size;
+    }
+
+    loop {
+    }
+}
